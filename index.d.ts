4246d4383fa35e654c4003dde74a790598c70368
dygapp <dygapp@qq.com>
2018-03-26 08:30:03 +0800

    0.0.3 修改index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,31 +1,29 @@
 
-declare namespace Naf.Weixin {
-
-  declare interface MessageData {
-    toUser: string;
-    fromUser: string;
-    msgType: string;
-  }
-
-  declare interface EventData {
-    event: string;
-    eventKey: string;
-    ticket: string;
-  }
-
-  export interface MessageUtil {
-
-    static extractFromXml(xmlData: string): MessageData;
-
-    static contentFromXml(xmlData: string): string;
-
-    static recognitionFromXml(xmlData: string): string;
-
-    static eventFromXml(xmlData: string): EventData;
-
-    static tryParse(xmlData: string, regex: RegExp): string;
-
-    static generateXml(toUser: string, fromUser: string, textContent: string): string;
-  }
+declare interface MessageData {
+  toUser: string;
+  fromUser: string;
+  msgType: string;
 }
 
+declare interface EventData {
+  event: string;
+  eventKey: string;
+  ticket: string;
+}
+
+export class MessageUtil {
+
+  static extractFromXml(xmlData: string): MessageData;
+
+  static contentFromXml(xmlData: string): string;
+
+  static recognitionFromXml(xmlData: string): string;
+
+  static eventFromXml(xmlData: string): EventData;
+
+  static tryParse(xmlData: string, regex: RegExp): string;
+
+  static generateXml(toUser: string, fromUser: string, textContent: string): string;
+}
+
+export as namespace NafWeixin;
